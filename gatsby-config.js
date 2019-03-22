4db723368c37d93981481989690b0db75356409a
Sean Fairchild <Sean.Fairchild@bhnetwork.com>
2019-03-22 11:43:39 -0400

    add more defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dev Tips`,
+    description: `Short developer tips that I find handy`,
+    author: `@logicSean`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hi Everyone</h1>
+    <p>Welcome to my site. This is still in development.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
